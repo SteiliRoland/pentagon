d37094306dc3ecd11da4f78d12bac19a09adddc3
SteiliRoland <steiliroland2001@gmail.com>
2022-01-09 20:07:16 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
+/*
+* File: App.js
+* Author: Steili Roland
+* Copyright: 2022, Steili Roland
+* Group: Szoft II/N
+* Date: 2022-01-09
+* Github: https://github.com/SteiliRoland/pentagon
+* Licenc: GNU GPL
+*/
+
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+console.log("Leírás: A program kiszámolja egy szabályos ötszög területét\nNév: Steili Roland\nDátum: 2022.01.09.");
 
 export default function App() {
+
+  const [side, setSide] = useState(0);
+  const [area, setArea] = useState(0);
+
+  function calcArea(){
+    let result = (5*Math.pow(side, 2)*Math.tan(54*Math.PI/180))/4;
+    setArea(parseFloat(result).toFixed(2));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text >Pentagon</Text>
+      <View >
+        <Text >Oldal:</Text>
+        <TextInput placeholder="oldal" onChangeText={ side => setSide(side) } />
+        <Button title="Számít" onPress={calcArea}/>
+      </View>
+      <Text >Terület: {area}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +44,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
